08954c43c05a568e2418804293833de169e4f650
Elisey <eliseykor@icloud.com>
2023-12-19 18:24:23 +0300

    add vk widgets

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  head: {
+    script: [
+      {
+        src: "https://vk.com/js/api/openapi.js?169",
+        type: "text/javascript"
+      }
+    ]
+  }
 })
