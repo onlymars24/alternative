7b72772df9a09b63da8c7248bb15ff309cdaddd4
Elisey <eliseykor@icloud.com>
2023-12-22 19:16:18 +0300

    fix config

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -3,13 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  head: {
-    script: [
-      {
-        src: "https://vk.com/js/api/openapi.js?169",
-        type: "text/javascript"
-      }
-    ]
-  }
+  plugins: [vue()]
 })
