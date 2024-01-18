f890520db15f66c67fcf2edf48818b9f938fee9f
onlymars24 <marsel.galimov.241@gmail.com>
2024-01-18 18:37:37 +0300

    c87

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -18,16 +18,44 @@ class FeedbackController extends Controller
             'descr' => $request->descr,
         ]);
 
-        Mail::to(env('MAIL_FEEDBACK'))->send(new FeedbackMail($request->name, $request->phone, $request->email, $request->topic, $request->descr));
+        Mail::to(env('MAIL_FEEDBACK1'))->send(new FeedbackMail($request->name, $request->phone, $request->email, $request->topic, $request->descr));
         return response([
             'feedback' => $feedback
         ]);
     }
 
     public function get(){
-        $feedback = Feedback::all();
+        $feedback = Feedback::orderByDesc('id')->get();
         return response([
             'feedback' => $feedback
+        ]);
+    }
+
+    // POST
+    // id
+    // comment
+    public function editComment(Request $request){
+        $feedback = Feedback::find($request->id);
+        $feedback->comment = $request->comment;
+        $feedback->save();
+        return response([
+            'feedback' => $feedback,
+        ]);
+    }
+
+    // POST
+    // id
+    // status
+    public function editStatus(Request $request){
+        $feedback = Feedback::find($request->id);
+        if($feedback->status != $request->status){
+            $history = json_decode($feedback->history);
+            $history[] = ['date' => date('Y-m-d H:i:s'), 'status' => $request->status ];
+        }
+        $feedback->status = $request->status;
+        $feedback->save();
+        return response([
+            'feedback' => $feedback,
         ]);
     }
 }
