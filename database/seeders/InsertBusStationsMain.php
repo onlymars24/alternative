7a670e2e6334e2ad1a251f5d15019c626de2538c
onlymars24 <marsel.galimov.241@gmail.com>
2024-07-05 20:04:47 +0300

    c195

--- database/seeders/InsertBusStationsMain.php
+++ database/seeders/InsertBusStationsMain.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\Setting;
+use App\Models\BusStation;
+use App\Models\DispatchPoint;
 use Illuminate\Database\Seeder;
+use App\Services\FtpLoadingService;
+use Illuminate\Support\Facades\File;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class InsertBusStationsMain extends Seeder
@@ -16,12 +20,57 @@ class InsertBusStationsMain extends Seeder
     public function run()
     {
         $busStationsMain = Setting::where('name', 'busStationsMain')->first();
-        if($busStationsMain){
-            $busStationsMain->delete();
+
+
+        $dispatchPoints = DispatchPoint::all();
+        // $dispatchPoint = DispatchPoint::find(73707);
+        // dd(!$dispatchPoint->bus_stations->count());
+        $busStationsSetting = [];
+        foreach($dispatchPoints as $dispatchPoint){
+          if(!$dispatchPoint->bus_stations->count()){
+            $busStation = BusStation::create([
+                'title' => $dispatchPoint->name,
+                'name' => 'Автовокзал '.$dispatchPoint->name,
+                'dispatch_point_id' => $dispatchPoint->id,
+                'hidden' => false,
+            ]);
+      
+            $newLoc = env('FRONTEND_URL').'/автовокзал/'.$busStation->title;
+            $xml = simplexml_load_file(env('XML_FILE_NAME'));
+            for($i = 0; $i < count($xml->url); $i++){
+                $xml->url[$i]->lastmod = date('Y-m-d');
+            }
+            $xmlExist = false;
+            // for($i = 0; $i < count($xml->url); $i++){
+            //     if($xml->url[$i]->loc == $newLoc){
+            //         $xmlExist = true;
+            //         break;
+            //     }
+            // }
+            if(!$xmlExist){
+              $newNode = $xml->addChild('url');
+              $newNode->addChild('loc', $newLoc);
+              $newNode->addChild('lastmod', date('Y-m-d'));
+              $newNode->addChild('changefreq', 'weekly');
+              $newNode->addChild('priority', '1.0');
+              File::put(env('XML_FILE_NAME'), $xml->asXML());
+            }
+          }
+          $busStationsSetting[$dispatchPoint['region']][] = $dispatchPoint->toArray();
         }
-        Setting::create([
-            'name' => 'busStationsMain',
-            'data' => '[]'
-        ]);
+        FtpLoadingService::put();
+      
+      
+        foreach($busStationsSetting as $key => $region){
+          usort($region, function($a, $b) {
+            return strcmp($a['name'], $b['name']);
+          });
+          $busStationsSetting[$key] = $region;
+        }
+        ksort($busStationsSetting);
+      
+        $busStationsMain = Setting::where('name', 'busStationsMain')->first();
+        $busStationsMain->data = json_encode(json_decode(json_encode($busStationsSetting)));
+        $busStationsMain->save();
     }
 }
