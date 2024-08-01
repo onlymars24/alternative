3e4ee3270cd102478433c439fdf65654b54a8e62
onlymars24 <marsel.galimov.241@gmail.com>
2024-08-01 17:56:24 +0300

    c211

---
+++ database/migrations/2024_08_01_175438_make_fields_nullable_in_cache_arrival_points.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cache_arrival_points', function (Blueprint $table) {
+            $table->string('region')->nullable()->change();
+            // $table->string('details')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cache_arrival_points', function (Blueprint $table) {
+            $table->string('region')->change();
+        });
+    }
+};
